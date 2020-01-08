2eb12122545d924beaec15bd713c08ca02726b09
Kalaputs, Andrei <akalaputs@scnsoft.com>
2020-01-08 20:28:23 +0300

    User search service injection to the REST Resource.

--- src/main/java/by/kalaputs/kitchen/duty/rest/UserSearchResource.java
+++ src/main/java/by/kalaputs/kitchen/duty/rest/UserSearchResource.java
@@ -1,13 +1,25 @@
 package by.kalaputs.kitchen.duty.rest;
 
+import com.atlassian.jira.bc.user.search.UserSearchService;
 import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
 
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/message")
+@Path("/user")
 public class UserSearchResource {
+    private UserSearchService userSearchService;
+
+    @Inject
+    public UserSearchResource(UserSearchService userSearchService) {
+        this.userSearchService = userSearchService;
+    }
+
+    public UserSearchResource() {
+    }
+
     @GET
     @AnonymousAllowed
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
