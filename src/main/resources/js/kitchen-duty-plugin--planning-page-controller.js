63821d1e659b30db4ed82f0d99c6866ece608e1e
Kalaputs, Andrei <akalaputs@scnsoft.com>
2020-01-11 01:26:25 +0300

    User search JS controller is done.

--- src/main/resources/js/kitchen-duty-plugin--planning-page-controller.js
+++ src/main/resources/js/kitchen-duty-plugin--planning-page-controller.js
@@ -1,8 +1,54 @@
-require(['aui/flag'], function (flag) {
-    AJS.flag({
-        type: 'success',
-        title: 'Kitchen Duty Plugin',
-        body: 'JS Controller is working',
-        close: "auto"
+const showSuccessFlag = function (message) {
+    require(['aui/flag'], function (flag) {
+        var myFlag = flag({
+            type: 'success',
+            title: 'Kitchen Duty Plugin',
+            close: 'auto',
+            body: message
+        });
     });
+};
+
+const initUserSearch = function (restUrl) {
+    const templateUserSearch = JIRA.Templates.KDP.userSearch();
+
+    const auiUserSelectOptions = {
+        ajax: {
+            url: restUrl + '/user/search',
+            dataType: 'json',
+            delay: 250,
+            data: function (searchTerm) {
+                return {
+                    query: searchTerm
+                };
+            },
+            results: function (data) {
+                return {
+                    results: data
+                };
+            },
+            cache: true
+        },
+        minimumInputLength: 1,
+        tags: 'true'
+    };
+
+    /* INIT TEMPLATES AND WIDGETS */
+
+    AJS.$('#kdp-planning-page-container').append(templateUserSearch);
+    AJS.$('#kdp-user-select').auiSelect2(auiUserSelectOptions);
+    AJS.$('#kdp-user-select-form').submit(function (e) {
+        e.preventDefault();
+        AJS.$(AJS.$('#kdp-user-select').select2('data')).each(function () {
+            showSuccessFlag(this.id);
+        });
+    });
+};
+
+AJS.toInit(function(){
+    AJS.log('KDP: Planning Page Controller initializing ...');
+    const baseUrl = AJS.params.baseURL;
+    const restUrl = baseUrl + '/rest/kitchenduty/1.0';
+
+    initUserSearch(restUrl);
 });
